56b0334d00673608d1bbfc8c7ee059dbf9bcdd28
Md.Mehedi Hasan <35430592+mehedi6892@users.noreply.github.com>
2019-08-31 22:40:51 +0600

    Create saarc country.py

---
+++ Basic Problem/saarc country.py
@@ -0,0 +1,11 @@
+saarc=["Bangladesh", "Afghanistan", "Bhutan", "Nepal", "India", "Pakistan", "Sri Lanka"]
+print(saarc[0])
+print("Number of country in saarc is: ", len(saarc))
+
+country= input("Enter The country name: ")
+if country in saarc:
+    print("country "  "is a number of saarc.")
+else:
+    print("country " "is a not a number of saarc. ")
+
+
