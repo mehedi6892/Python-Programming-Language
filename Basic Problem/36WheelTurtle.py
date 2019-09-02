48593959f6b599b73bd81988b6cf0b0e20350cef
Md.Mehedi Hasan <35430592+mehedi6892@users.noreply.github.com>
2019-09-03 01:29:08 +0600

    Create 36WheelTurtle.py

---
+++ Basic Problem/36WheelTurtle.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.color("blue")
+turtle.speed(10)
+
+counter=0
+while counter <36:
+     for i in range(4):
+          turtle.forward(100)
+          turtle.right(90)
+     turtle.right(10);
+     counter += 1;
+
+turtle.exitonclick()
